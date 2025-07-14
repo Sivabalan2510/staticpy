27c3b54d404259143d7aa2deac4a29ac9509ceef
Sivabalan .K <154497614+Sivabalan2510@users.noreply.github.com>
2025-07-14 16:53:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 from flask import Flask, Response
 from azure.storage.blob import BlobServiceClient
 import os
+import mimetypes
 
 app = Flask(__name__)
 
 AZURE_STORAGE_CONNECTION_STRING = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
-CONTAINER_NAME = "web"
+CONTAINER_NAME = "$web"
 blob_service_client = BlobServiceClient.from_connection_string(AZURE_STORAGE_CONNECTION_STRING)
 
-@app.route('/<site>/<path:file_path>')
-def serve_static(site, file_path):
-    blob_path = f"{site}/{file_path}"
+@app.route('/<path:file_path>')
+def serve_static(file_path):
     try:
-        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=blob_path)
+        blob_client = blob_service_client.get_blob_client(container=CONTAINER_NAME, blob=file_path)
         blob_data = blob_client.download_blob().readall()
-        return Response(blob_data, mimetype='text/html')
+        mime_type, _ = mimetypes.guess_type(file_path)
+        return Response(blob_data, mimetype=mime_type or 'application/octet-stream')
     except Exception as e:
         return f"Error fetching file: {str(e)}", 404
 
